765a2ca07761ac3be14a831015f454f0786fb326
Roman Ryltsov <roman@alax.info>
2023-08-27 14:51:19 +0200

    Multiple enhancements (randomization, statistics, tries)

--- BullCowGame.cpp
+++ BullCowGame.cpp
@@ -6,37 +6,85 @@
 
 int main()
 {
+    // NOTE: 0518 without randomization
+    std::srand(static_cast<unsigned int>(std::time(0))); // https://en.cppreference.com/w/cpp/numeric/random/srand
     Game Game;
-    Game.Secret.Create();
-    //std::cout << "Secret: " << Game.Secret.ToString() << std::endl;
+    Game.Reset();
     for(; ; )
     {
         std::string Input;
         std::getline(std::cin, Input);
+        if(Input.empty())
+            continue;
         if(Input == "q") // Quit
             break;
         if(Input == "r") // Restart
         {
-            Game.Secret.Create();
-            Game.QuestionVector.clear();
+            Game.Reset();
             std::cout << "-- Restarted, new game" << std::endl;
+            continue;
+        }
+        if(Input == "??") // Secret
+        {
+            std::cout << "Secret is " << Game.Secret.ToString() << std::endl;
+            continue;
+        }
+        if(Input == "?") // Remaining matches, if 16 or less
+        {
+            std::cout << Game.MatchVector.size() << " remaining" << std::endl;
+            static size_t constexpr const MatchSizeThreshold = 16;
+            if(Game.MatchVector.size() <= MatchSizeThreshold)
+                for(auto&& Match: Game.MatchVector)
+                    std::cout << "  " << Match.ToString() << std::endl;
+            continue;
+        }
+        if(Input.substr(0, 1) == "/") // Try
+        {
+            Number TryQuestion;
+            if(!TryQuestion.TryParse(Input.substr(1)))
+            {
+                std::cout << "Not a valid question " << Input.substr(1) << std::endl;
+                continue;
+            }
+            std::cout << "--" << std::endl;
+            bool Match = true;
+            for(auto&& Question: Game.QuestionVector)
+            {
+                auto const Answer = Game.Secret.Ask(Question);
+                auto const TryAnswer = TryQuestion.Ask(Question);
+                std::cout << Question.ToString() << " - " << Answer.ToString() << " - " << TryAnswer.ToString() << " against yours" << std::endl;
+                Match &= Answer == TryAnswer;
+            }
+            if(Match)
+                std::cout << TryQuestion.ToString() << " matches, go ahead" << std::endl;
             continue;
         }
         Number Question;
         if(!Question.TryParse(Input))
         {
-            std::cout << "Not a valid question: " << Input << std::endl;
+            std::cout << "Not a valid question " << Input << std::endl;
             continue;
         }
         auto const Answer = Game.Secret.Ask(Question);
         Game.QuestionVector.emplace_back(Question);
         std::cout << "--" << std::endl;
+        auto MatchVector = Number::All();
         for(auto&& Question: Game.QuestionVector)
-            std::cout << Question.ToString() << " " << Game.Secret.Ask(Question).ToString() << std::endl;
+        {
+            auto const Answer = Game.Secret.Ask(Question);
+            std::vector<Number> NewMatchVector;
+            std::for_each(MatchVector.cbegin(), MatchVector.cend(), [&] (auto&& Match)
+            { 
+                if(Game.Secret.Ask(Question) == Match.Ask(Question))
+                    NewMatchVector.emplace_back(Match);
+            });
+            std::cout << Question.ToString() << " - " << Answer.ToString() << " - " << MatchVector.size() << " -> " << NewMatchVector.size() << std::endl;
+            MatchVector = std::move(NewMatchVector);
+        }
+        Game.MatchVector = MatchVector;
         if(Answer.Bulls == Question.ValueSize)
         {
-            Game.Secret.Create();
-            Game.QuestionVector.clear();
+            Game.Reset();
             std::cout << "-- Done, new game" << std::endl;
         }
     }
@@ -47,8 +95,6 @@ int main()
 
 - colorization
 - output with emojis, such as https://emojipedia.org/keycap-digit-zero
-- "?" + number pre-shows question against previous questions
-- display how many combinations remain valid
 - "+" and "-" to change your number card, "*" to reset it
 
 */
--- Engine.h
+++ Engine.h
@@ -3,9 +3,20 @@
 #include <cassert>
 #include <vector>
 #include <string>
+#include <algorithm>
+#include <utility>
 
 struct Answer
 {
+	bool operator==(Answer const& Value) const
+	{
+		return Bulls == Value.Bulls && Cows == Value.Cows;
+	}
+	bool operator!=(Answer const& Value) const
+	{
+		return !(*this == Value);
+	}
+
 	std::string ToString() const
 	{
 		char Result[16];
@@ -48,11 +59,13 @@ struct Number
 	}
 	bool Valid() const
 	{
+		static_assert(ValueSize == sizeof Value / sizeof *Value);
+		static_assert(ValueSize <= 10);
 		for(auto&& Element : Value)
 			if(Element < '0' || Element > '9')
 				return false;
 		for(size_t IndexA = 0; IndexA < std::size(Value) - 1; IndexA++)
-			for(size_t IndexB = IndexA + 1; IndexB < std::size(Value) - 1; IndexB++)
+			for(size_t IndexB = IndexA + 1; IndexB < std::size(Value); IndexB++)
 				if(Value[IndexA] == Value[IndexB])
 					return false;
 		return true;
@@ -81,13 +94,46 @@ struct Number
 		assert(Answer.Bulls + Answer.Cows <= std::size(Value));
 		return Answer;
 	}
+	static std::vector<Number> All()
+	{
+		std::vector<Number> Result;
+		Number Value;
+		for(size_t Index = 0; Index < ValueSize; Index++)
+			Value.Value[Index] = static_cast<char>('0' + Index);
+		assert(Value.Valid());
+		Result.emplace_back(Value);
+		for(; ; )
+		{
+			size_t Index = ValueSize;
+			for(; Index > 0; Index--)
+			{
+				Value.Value[Index - 1]++;
+				if(Value.Value[Index - 1] <= '9')
+					break;
+				Value.Value[Index - 1] = '0';
+			}
+			if(Index == 0)
+				break;
+			if(Value.Valid())
+				Result.emplace_back(Value);
+		}
+		return Result;
+	}
 
 	static size_t constexpr const ValueSize = 4;
-	char Value[4];
+	char Value[ValueSize];
 };
 
 struct Game
 {
+	void Reset()
+	{
+		Secret.Create();
+		QuestionVector.clear();
+		MatchVector = Number::All();
+	}
+
 	Number Secret;
 	std::vector<Number> QuestionVector;
+	std::vector<Number> MatchVector;
 };
